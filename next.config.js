1c6125db279fd5fe547f383797a6acb7b122abdf
zynmurel <comingues99@gmail.com>
2025-07-31 21:57:12 +0800

    config

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ import "./src/env.js";
 /** @type {import("next").NextConfig} */
 const config = {
     images : {
-        domains  : ['zzwlrwodwcijsendalzt.supabase.co', 'https://mqzjaptjvjyvdfogmqmg.supabase.co']
+        domains  : ['zzwlrwodwcijsendalzt.supabase.co', 'mqzjaptjvjyvdfogmqmg.supabase.co']
     },
 };
 
