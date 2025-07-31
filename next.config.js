e60874478b8c38c6c6c764c49f7ee47dcc7125a1
zynmurel <comingues99@gmail.com>
2025-07-31 21:11:11 +0800

    for upload

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ import "./src/env.js";
 /** @type {import("next").NextConfig} */
 const config = {
     images : {
-        domains  : ['zzwlrwodwcijsendalzt.supabase.co']
+        domains  : ['zzwlrwodwcijsendalzt.supabase.co', 'https://mqzjaptjvjyvdfogmqmg.supabase.co']
     },
 };
 
--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,7 +1,7 @@
 // utils/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://zzwlrwodwcijsendalzt.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp6d2xyd29kd2NpanNlbmRhbHp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk4ODQ0NjEsImV4cCI6MjA2NTQ2MDQ2MX0.pqlhdqwO7bUPwMBmOasohTExZj6GnQbuoobYtds6qo0";
+const supabaseUrl = "https://mqzjaptjvjyvdfogmqmg.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1xemphcHRqdmp5dmRmb2dtcW1nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM5NjI4NTEsImV4cCI6MjA2OTUzODg1MX0.yvT9BrZSGJk-sA7o-RRcf1Be_NIt7BAVLG7jPx9IyEw";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
