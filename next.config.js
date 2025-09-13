cbd8fe1d79d63e27907f91831bdfbd08d4adcfbb
zynmurel <comingues99@gmail.com>
2025-09-13 12:05:42 +0800

    image url fix

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ import "./src/env.js";
 /** @type {import("next").NextConfig} */
 const config = {
     images : {
-        domains  : ['zzwlrwodwcijsendalzt.supabase.co', 'mqzjaptjvjyvdfogmqmg.supabase.co']
+        domains  : ['zzwlrwodwcijsendalzt.supabase.co', 'mqzjaptjvjyvdfogmqmg.supabase.co','wvidpdhwvpzhqnmevole.supabase.co']
     },
 };
 
